5704105bb501bc13869660a0ae0928c40baea4b9
YD-Yang <48411346+YD-Yang@users.noreply.github.com>
2019-09-22 22:57:31 -0500

    Update TSNE.py

--- TSNE.py
+++ TSNE.py
@@ -1,3 +1,3 @@
 from sklearn.manifold import TSNE
 
-med_tsne = TSNE(n_components=3, random_state=123).fit_transform(med_dummies)
+med_tsne = TSNE(n_components=3, random_state=123).fit_transform(df_dummies)
